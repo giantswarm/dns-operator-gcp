8288861e47415cbe074821e1dc55606427320f09
Mario Nitchev <mario@giantswarm.io>
2022-06-02 17:01:13 +0200

    Ignore non-LoadBalancer services in ingress registrar (#14)
    * Ignore non-LoadBalancer Services for ingress record
    
    The nginx ingress app installs multiple ClusterIP services labeled the
    same way as the LoadBalancer one. Those need to be ignored.
    
    Also move the logic getting the IP from the service to the (now)
    LoadBalancer client
    
    * Update Changelog
    
    * Refactor LB client

--- pkg/registrar/ingress.go
+++ pkg/registrar/ingress.go
@@ -8,7 +8,6 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/go-logr/logr"
 	clouddns "google.golang.org/api/dns/v1"
-	corev1 "k8s.io/api/core/v1"
 	capg "sigs.k8s.io/cluster-api-provider-gcp/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -21,38 +20,38 @@ const (
 	EndpointIngress  = "ingress"
 )
 
-//counterfeiter:generate . ServiceClient
-type ServiceClient interface {
-	GetByLabel(context.Context, string, string) (corev1.Service, error)
+//counterfeiter:generate . LoadBalancerClient
+type LoadBalancerClient interface {
+	GetIPByLabel(context.Context, string, string) (string, error)
 }
 
 type Ingress struct {
-	baseDomain    string
-	serviceClient ServiceClient
-	dnsService    *clouddns.Service
+	baseDomain         string
+	loadBalancerClient LoadBalancerClient
+	dnsService         *clouddns.Service
 }
 
-func NewIngress(baseDomain string, dnsService *clouddns.Service, serviceClient ServiceClient) *Ingress {
+func NewIngress(baseDomain string, dnsService *clouddns.Service, serviceClient LoadBalancerClient) *Ingress {
 	return &Ingress{
-		baseDomain:    baseDomain,
-		serviceClient: serviceClient,
-		dnsService:    dnsService,
+		baseDomain:         baseDomain,
+		loadBalancerClient: serviceClient,
+		dnsService:         dnsService,
 	}
 }
 
 func (r *Ingress) Register(ctx context.Context, cluster *capg.GCPCluster) error {
-	logger := log.FromContext(ctx)
-	logger = logger.WithName("ingress-registrar")
+	logger := r.getLogger(ctx)
 
 	logger.Info("Registering record")
 	defer logger.Info("Done registering record")
 
-	ingressDomain := fmt.Sprintf("%s.%s.%s.", EndpointIngress, cluster.Name, r.baseDomain)
-	ingressIP, err := r.getLoadBalancerIP(ctx, logger)
+	ingressIP, err := r.loadBalancerClient.GetIPByLabel(ctx, LabelIngressKey, LabelIngressValue)
 	if err != nil {
+		logger.Error(err, "Failed to get LoadBalancer ip")
 		return microerror.Mask(err)
 	}
 
+	ingressDomain := fmt.Sprintf("%s.%s.%s.", EndpointIngress, cluster.Name, r.baseDomain)
 	record := &clouddns.ResourceRecordSet{
 		Name: ingressDomain,
 		Rrdatas: []string{
@@ -89,37 +88,6 @@ func (r *Ingress) Unregister(ctx context.Context, cluster *capg.GCPCluster) erro
 	}
 
 	return microerror.Mask(err)
-}
-
-func (r *Ingress) getLoadBalancerIP(ctx context.Context, logger logr.Logger) (string, error) {
-	service, err := r.serviceClient.GetByLabel(ctx, LabelIngressKey, LabelIngressValue)
-	if err != nil {
-		logger.Error(err, "Failed to get LoadBalancer service account")
-		return "", err
-	}
-
-	if service.Spec.Type != corev1.ServiceTypeLoadBalancer {
-		logger.Error(err,
-			"Service not LoadBalancer type",
-			"service.name", service.Name,
-			"service.namespace", service.Namespace,
-		)
-		return "", fmt.Errorf("found %s Service, expected type LoadBalancer", service.Spec.Type)
-	}
-
-	if len(service.Status.LoadBalancer.Ingress) != 1 {
-		logger.Error(err,
-			"Found more than one Load Balancer ingresses",
-			"service.name", service.Name,
-			"service.namespace", service.Namespace,
-		)
-		return "", fmt.Errorf(
-			"found %d LoadBalancer ingresses, expected 1",
-			len(service.Status.LoadBalancer.Ingress),
-		)
-	}
-
-	return service.Status.LoadBalancer.Ingress[0].IP, nil
 }
 
 func (r *Ingress) getLogger(ctx context.Context) logr.Logger {
